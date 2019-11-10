230acb4f76d1692c3108554a1e06e65f4014c880
sandfishfactory <sandfishfactory@gmail.com>
2019-11-10 22:08:51 +0900

    realmを利用したコードの追加

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
 <template>
-  <app-navigator></app-navigator>
+<app-navigator></app-navigator>
 </template>
 
 <script>
 import AppNavigator from './router'
+import Realm from 'realm'
+
+
 export default {
-  components: { AppNavigator },
+  components: {
+    AppNavigator
+  },
+  created: function() {
+    const UserSchema = {
+      name: 'User',
+      properties: {
+        userid: 'string',
+        password: 'string',
+      }
+    };
+
+    Realm.open({
+        schema: [UserSchema]
+      })
+      .then(realm => {
+        // Realmオブジェクトを作成してローカルDBに保存します
+        realm.write(() => {
+          const userData = realm.create('User', {
+            userid: 'Yamada',
+            password: 'YamadaPassword',
+          });
+        });
+        const users = realm.objects('User');
+        console.log("[DEBUG]users:" + JSON.stringify(users))
+      });
+  }
 }
 </script>
